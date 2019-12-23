2187113b560a02da74b669efb7aadbf63659c216
PetarBM <petar.balaic@gmail.com>
2019-12-23 15:54:08 +0100

    first two days

---
+++ Day01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DedMraz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("../input.txt");
+            int[] moduleFueles = new int[lines.Length];         
+            int[] totalFuels = new int[lines.Length];
+            int sum = 0;
+            int totalSum = 0;
+
+            for(int i = 0; i < lines.Length ;i++)
+            {
+                int fuel = int.Parse(lines[i]);             
+                moduleFueles[i] = fuel / 3 - 2;
+                totalFuels[i] = CalculateTotalFueles(fuel);
+            }
+
+            foreach (int moduleFuel in moduleFueles)
+            {
+                sum += moduleFuel; 
+            }
+      
+            foreach(int totalFuel in totalFuels)
+            {
+                totalSum += totalFuel;
+            }
+         
+            Console.WriteLine(sum);
+            Console.WriteLine(totalSum);
+        }
+       
+        static int CalculateTotalFueles(int fuel)
+        {
+            fuel = fuel / 3 - 2;
+            if(fuel <= 0)
+            {
+                return 0;
+            }
+            return fuel + CalculateTotalFueles(fuel);
+        }
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace DedMraz02
+{
+    class Program
+    {
+        static string[] inputs;
+        static int[] inputCodes;
+        static bool flag;
+
+        static void Main(string[] args)
+        {
+            inputs = System.IO.File.ReadAllText("input.txt").Split(',');
+            inputCodes = new int[inputs.Length];
+            SetParameters();
+            TryParameters();        
+        }
+
+        private static void SetParameters()
+        {
+            ResetMemory();
+            inputCodes[1] = 12;
+            inputCodes[2] = 2;
+            Run();
+            Console.WriteLine(inputCodes[0]);
+        }
+
+        private static void TryParameters()
+        {
+            for(int i=0; i < 100; i++)
+            {
+                for(int j = 0; j < 100; j++)
+                {
+                    ResetMemory();
+                    inputCodes[1] = i;
+                    inputCodes[2] = j;
+                    Run();
+                    if (inputCodes[0] == 19690720)
+                    {
+                        Console.WriteLine(i*100+j);
+                        return;
+                    }
+                }
+
+            }
+        }
+
+        private static void Run()
+        {
+            flag = false;
+            for (int i = 0; i < inputCodes.Length; i += 4)
+            {
+                switch (inputCodes[i])
+                {
+                    case 1:
+                        inputCodes[inputCodes[i + 3]] = inputCodes[inputCodes[i + 1]] + inputCodes[inputCodes[i + 2]];
+                        break;
+                    case 2:
+                        inputCodes[inputCodes[i + 3]] = inputCodes[inputCodes[i + 1]] * inputCodes[inputCodes[i + 2]];
+                        break;
+                    case 99:
+                        flag = true;
+                        break;
+                    default:
+                        Console.WriteLine("nEŠT PO Krivu Stari!");
+                        flag = true;
+                        break;
+                }
+                if (flag)
+                {
+                    break;
+                }
+            }
+        }
+
+        private static void ResetMemory()
+        {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                inputCodes[i] = int.Parse(inputs[i]);
+            }
+        }
+    }
+}
